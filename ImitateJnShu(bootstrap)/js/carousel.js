47528d68a66f5d33d7f0a041fdadd8d17d67ea46
byalice <1533674340@qq.com>
2016-08-02 01:23:42 +0800

    carousel

--- ImitateJnShu(bootstrap)/js/carousel.js
+++ ImitateJnShu(bootstrap)/js/carousel.js
@@ -1,115 +1,73 @@
 /**
- * Created by alice on 2016/7/19.
+ * Created by alice on 2016/8/2.
  */
-/**
- * Created by alice on 2016/7/19.
- */
-//全屏滚动
-$(function(){
-    var n=0;
-    var imgLength=$(".b-img a").length;
-    var ctWidth=imgLength*100;
-    var itemWidth=1/imgLength*100;
-    $(".b-img").width(ctWidth+"%");
-    $(".b-img a").width(itemWidth+"%");
-    $(".b-list").width(imgLength*30);
-    if(imgLength>1)
-    {
-        for(var i=0;i<imgLength;i++)
-        {
-            var listSpan=$("<span></span>")
-            $(".b-list").append(listSpan);
+window.onload = function () {
+    var oDiv = document.getElementById('play');
+    var aBtn = oDiv.getElementsByTagName('ol')[0].getElementsByTagName('li');
+    var oPrev = document.getElementById('prev');
+    var oNext = document.getElementById('next');
+    var oImg = document.getElementById('img');
+    var arrUrl =[
+        "img/banner-1.jpg",
+        "img/banner-2.jpg",
+        "img/banner-3.jpg",
+        "img/banner-4.jpg"
+    ];
+
+    /*数字与图片序列号相对应*/
+    var now = 0;
+    for (var i = 0; i < aBtn.length; i++) {
+        aBtn[i].index = i;     //把按钮序号赋予i
+        aBtn[i].onclick = function () {
+            now = this.index;
+            tab();
         }
     }
-    $(".b-list span:eq(0)").addClass("spcss").siblings("span").removeClass("spcss");
-    $(".bar-right em").click(function(){
-        if(n==imgLength-1)
-        {
-            var ctPosit=(n+1)*100;
-            $(".banner").append($(".b-img").clone());
-            $(".b-img:last").css("left","100%");
-            $(".b-img:first").animate({"left":"-"+ctPosit+"%"},1000);
-            $(".b-img:last").animate({"left":"0"},1000);
-            var setTime0=setTimeout(function () {
-                $(".banner .b-img:first").remove();
-            }, 1000);
-            n=0;
-            $(".b-list span:eq("+n+")").addClass("spcss").siblings("span").removeClass("spcss");
+
+    /*当前所在卡片与图片保持一致*/
+    function tab() {
+        for (var i = 0; i < aBtn.length; i++) {
+            aBtn[i].className = '';
         }
-        else
-        {
-            n++;
-            var ctPosit=n*100;
-            $(".b-img").animate({"left":"-"+ctPosit+"%"},1000);
-            $(".b-list span:eq("+n+")").addClass("spcss").siblings("span").removeClass("spcss");
-        }
-    })
-    $(".bar-left em").click(function(){
-        if(n==0)
-        {
-            var stPosit=imgLength*100;
-            var etPosit=(imgLength-1)*100;
-            $(".banner").prepend($(".b-img").clone());
-            $(".b-img:first").css("left","-"+stPosit+"%");
-            $(".b-img:last").animate({"left":"100%"},1000);
-            $(".b-img:first").animate({"left":"-"+etPosit+"%"},1000);
-            var setTime0=setTimeout(function () {
-                $(".banner .b-img:last").remove();
-            }, 1000);
-            n=imgLength-1;
-            $(".b-list span:eq("+n+")").addClass("spcss").siblings("span").removeClass("spcss");
-        }
-        else
-        {
-            n--;
-            var ctPosit=n*100;
-            $(".b-img").animate({"left":"-"+ctPosit+"%"},1000);
-            $(".b-list span:eq("+n+")").addClass("spcss").siblings("span").removeClass("spcss");
-        }
-    })
-    $(".b-list span").click(function(){
-        var lsIndex=$(this).index();
-        n=lsIndex;
-        var ctPosit=n*100;
-        $(".b-img").animate({"left":"-"+ctPosit+"%"},1000);
-        $(this).addClass("spcss").siblings("span").removeClass("spcss");
-    })
-    function rollEnvent(){
-        if(n==imgLength-1)
-        {
-            var ctPosit=(n+1)*100;
-            $(".banner").append($(".b-img").clone());
-            $(".b-img:last").css("left","100%");
-            $(".b-img:first").animate({"left":"-"+ctPosit+"%"},1000);
-            $(".b-img:last").animate({"left":"0"},1000);
-            var setTime0=setTimeout(function () {
-                $(".banner .b-img:first").remove();
-            }, 1000);
-            n=0;
-            $(".b-list span:eq(0)").addClass("spcss").siblings("span").removeClass("spcss");
-        }
-        else
-        {
-            n++;
-            var ctPosit=n*100;
-            $(".b-img").animate({"left":"-"+ctPosit+"%"},1000);
-            $(".b-list span:eq("+n+")").addClass("spcss").siblings("span").removeClass("spcss");
-        }
+        aBtn[now].className = 'active';
+        oImg.src = arrUrl[now];
     }
-    var slidesetInterval=setInterval(rollEnvent,4000);
-    $(".banner").hover(function(){clearInterval(slidesetInterval);},function(){slidesetInterval=setInterval(rollEnvent,4000);});
-    $(".bar-left").mouseover(function(){
-        $(this).css("background","url(images/arr-bg.png)");
-        $(this).find("em").addClass("emcss");
-    }).mouseleave(function(){
-        $(this).css("background","none");
-        $(this).find("em").removeClass("emcss");
-    })
-    $(".bar-right").mouseover(function(){
-        $(this).css("background","url(images/arr-bg.png)");
-        $(this).find("em").addClass("emcss");
-    }).mouseleave(function(){
-        $(this).css("background","none");
-        $(this).find("em").removeClass("emcss");
-    })
-})
+
+    /*按下左右按钮时*/
+    oPrev.onclick = function () {
+        now--;
+        if (now == -1) {
+            now = aBtn.length-1;
+        }
+        tab();
+    };
+    oNext.onclick = function () {
+        now++;
+        if (now == aBtn.length) {
+            now = 0;
+        }
+        tab();
+    };
+
+
+
+
+    /*图片自动播放*/
+    function next() {
+        now++;
+        if (now == aBtn.length) {
+            now = 0;
+        }
+
+        setInterval(tab(),10000);
+    }
+
+    var timer = setInterval(next, 5000);
+    /*移入暂停，移出继续播放*/
+    oDiv.onmouseover = function () {
+        clearInterval(timer);
+    };
+    oDiv.onmouseout = function () {
+        timer = setInterval(next, 2000);
+    };
+};
